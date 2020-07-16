dd26dcb25d1660efe0edf006a1505f00c210ff18
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-16 12:23:45 +0200

    Update login.py

--- login.py
+++ login.py
@@ -7,22 +7,8 @@ class LoginData():
         self.tokenID =tokenID
 
 
-def loginRequest(client,loggedIn):
-    
-    loginRequest = "no" 
-    loginRequest = input("Do you want to use the vehicle? (yes/no)")    # wird ersetzt durch RFID
-    if loginRequest == "yes":
-        client.publish("/V4/loginRequest", loginRequest)
-        print("Login request sent to webserver")
-        loginAnswer = "validated"           # wird durch antwort von Webgruppe ersetzt"
-        if loginAnswer == "validated":
-            loggedIn = True
-            print("log in succesfull")
-            return loggedIn
-        else:
-            print("log in failed, you are not entiteled to use this vehicle!")
-            loggedIn = False
-            return loggedIn
+loginRequest = input("wollen sie sich einloggen")
+print(loginRequest)
 
 
 
