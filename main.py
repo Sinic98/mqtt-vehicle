05a2f6fe42e3140dfdcf179fe532bd0980533df1
Sinic98 <jannik.linke@web.de>
2020-07-16 09:50:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def saveSensorValuesAsJson(accel, magnet, gyro, alti):
     return json_data
 
 def values2db():
-    db = mysql.connector.connect(user='pi8', password='aut-pi8', host='192.168.200.108, database='mqttclient')
+    db = mysql.connector.connect(user='pi8', password='aut-pi8', host='192.168.200.108', database='mqttclient')
     print("MySQL connected")
 
 
