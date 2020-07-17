56b139f9c1bfa1dad6831f303c33a0dc33574681
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-17 16:48:11 +0200

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import mqtt_client
 
 def offlinehandler(connected, accel, magnet, gyro, alti, client):
     if not connected:
-        file = open("offline.txt", "w+")
+        file = open("offline.txt", "+")
         while not connected:
             json_data = sensors.saveSensorValuesAsJson(accel, magnet, gyro, alti)
             file.write(json_data)
