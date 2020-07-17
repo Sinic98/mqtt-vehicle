02494165e2ada0887e91710eafdb91eeed08216d
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-17 15:04:12 +0200

    spil

--- login.py
+++ login.py
@@ -7,10 +7,3 @@ class LoginData():
         self.tokenID =tokenID
 
 
-username1= input("User: ")
-password1= input("Password: ")
-id= "sssss"
-
-logindata=LoginData(timestamp= time.time()*1000, username=username1, password = password1, tokenID = id)
-logindatajson=json.dumps(logindata.__dict__)
-print(logindatajson)
