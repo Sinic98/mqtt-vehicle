3909522de412012b089acd80e892baf2fddcf56b
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-17 16:43:42 +0200

    Update database.py

--- database.py
+++ database.py
@@ -11,5 +11,6 @@ def offlinehandler(connected, accel, magnet, gyro, alti, client):
             file.write(json_data)
             time.sleep(1)
             offline_json = file.read()
+            print(offline_json)
             mqtt_client.publish("/SysArch/V4/offline", offline_json, client)
         file.close()
