380b82896e48e05cc47510987afd23b985a783e9
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-17 17:26:36 +0200

    Update login.py

--- login.py
+++ login.py
@@ -9,12 +9,12 @@ class LoginData():
 
 def loginRequest(client, loggedIn):
     if raw_input("Do you want to log in? (yes/no)") == 'yes':
-        login = LoginData(timestamp=time.time()*1000, tokenID="TOKENIDEINFUEGEN")
+        login = LoginData(timestamp=time.time()*1000, tokenID="TOKENID EINFUEGEN")
         login_json = json.dumps(login.__dict__)
         mqtt_client.publish("/SysArch/V4/LoginRequest", login_json, client)
         print("LoginRequest")
         loggedIn = True
-        time.sleep(0.5)
+        time.sleep(0.9)
         return loggedIn
     else:
         time.sleep(0.5)
