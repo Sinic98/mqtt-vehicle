2a4e676941a343fcc65d5a9f7631aa2325e581ea
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-17 17:09:58 +0200

    Update database.py

--- database.py
+++ database.py
@@ -5,12 +5,12 @@ import mqtt_client
 
 def offlinehandler(connected, accel, magnet, gyro, alti, client):
     if not connected:
-        file = open("offline.txt", "+")
+        file = open("offline.txt", "w+")
         while not connected:
             json_data = sensors.saveSensorValuesAsJson(accel, magnet, gyro, alti)
             file.write(json_data)
             time.sleep(1)
-            offline_json = file.read()
+            offline_json = file.readlines()
             print(offline_json)
             print("Hier sollte es stehen")
             mqtt_client.publish("/SysArch/V4/offline", offline_json, client)
