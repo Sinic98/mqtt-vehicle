27350d2cda1ddc9406584a3ed5648d70b987490d
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-17 15:23:27 +0200

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -22,7 +22,7 @@ def setupClient():
     client.on_publish = on_publish
 
     lwm = "Error: Client disconnected. Dataloss may occur!"     # last will message
-    client.will_set("/SysArch/V4", lwm, QOS1)
+    client.will_set("/SysArch/V4", lwm, 1)
     client.loop_start()
     return client
 
