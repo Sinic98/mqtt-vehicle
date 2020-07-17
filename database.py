31480a1cdf218b5752b6d4c304f6467e03df58a9
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-17 17:20:03 +0200

    Update database.py

--- database.py
+++ database.py
@@ -10,12 +10,9 @@ def offlinehandler(connected, accel, magnet, gyro, alti, client):
             json_data = sensors.saveSensorValuesAsJson(accel, magnet, gyro, alti)
             file.write(json_data)
             time.sleep(1)
-            json_data = sensors.saveSensorValuesAsJson(accel, magnet, gyro, alti)
-            file.write(json_data)
-            time.sleep(1)
-            connected = True
         file.close()
         offline_json = open("offline.txt").readlines()
         offline_json_str = str(offline_json)
         mqtt_client.publish("/SysArch/V4/offline", offline_json_str, client)
         file.close()
+        print("Offline data sent to: SysArch/V4/offline")
