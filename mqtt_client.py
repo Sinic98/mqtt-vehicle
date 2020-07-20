228d143cc4d56cdcfa5ec1cf2dae0c5b0fabe313
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-20 14:24:51 +0200

    asd

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,25 +1,27 @@
 import paho.mqtt.client as mqtt
 
-
+# create a MQTT client
 def setupClient():
-    # creates a MQTT Client
 
     client = mqtt.Client("Pi8")
-    client.connected_flag = False
-
     client.username_pw_set("V4", "DE7")
     client.connect("localhost", 8884)
     print("Client connected")
 
     def on_connect(client, userdata, flags, rc):
-        print("CONNACK received with code %d." % (rc))
-        client.connected_flag = True
-        client.disconnect_flag = False
+        if rc == 0:
+            print("CONNACK received with code %d." % (rc))
+            client.connected_flag = True
+            client.disconnect_flag = False
+        else:
+            print("Connection refused with code %d." %(rc))
+           # client.connected_flag = False
+           # client.disconnected_flag = True
 
     def on_disconnect(client, userdata, rc):
         logging.info("disconnecting reason  " + str(rc))
-        client.connected_flag = False
-        client.disconnect_flag = True
+        #client.connected_flag = False
+       # client.disconnect_flag = True
 
     def on_publish(client, userdata, rc):
         print("data published \n")
@@ -30,6 +32,7 @@ def setupClient():
 
     lwm = "Error: Client disconnected. Dataloss may occur!"     # last will message
     client.will_set("/SysArch/V4", lwm, 1, retain = True)
+    client.reconnect_delay_set(min_delay = 1, max_delay = 15)
     client.loop_start()
     return client
 
