dcfbb2bd2f6a99b14b0673ed5ff752544cd3d0f8
Sinic98 <jannik.linke@web.de>
2020-07-21 21:28:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ import rfid
 import sys
 import termios
 import tty
+from pynput import keyboard
 
 
 
@@ -51,7 +52,23 @@ def main():
         database.offlinehandler(client.connected_flag, accel, magnet, gyro, alti, client)
     time.sleep(0.1)
 
-    while loggedIn == True:
+    def on_press(key):
+        if key == keyboard.Key.esc:
+            return False  # stop listener
+        try:
+            k = key.char  # single-char keys
+        except:
+            k = key.name  # other keys
+        if k in ['1', '2', 'left', 'right']:  # keys of interest
+            # self.keys.append(k)  # store it in global-like variable
+            print('Key pressed: ' + k)
+            return False  # stop listener; remove this if want more keys
+
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()  # start to listen on a separate thread
+    listener.join()  # remove if main thread is polling self.keys
+
+    while loggedIn == False:
         key = inkey()
         if key == "q":
             mqtt_client.stopClient(client)
