5bd3da619c9598feb74e19ee75abbe1a4e586860
Sinic98 <jannik.linke@web.de>
2020-07-21 22:01:38 +0200

    gui

---
+++ gui.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+def idle():
+    print("Welcome, " + user)
+    print("***  MENU  *** \n\n")
+    print("~ 1-Car Stats ~")
+    print("~ 2-Air conditioning ~")
+    print("~ 3-Logout ~")
+
+    input = raw_input("Please enter the number of your choice: ")
+
+    if input == "0":
+        carstats()
+    elif input == "1":
+        aircond()
+    elif input =="2":
+        logout()
+
+def carstats(input):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Car stats")
+
+def aircond(input):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Air Cond")
+
+def logout(input):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Logout")
--- main.py
+++ main.py
@@ -21,7 +21,8 @@ import rfid
 import sys
 import termios
 import tty
-from pynput import keyboard
+import gui
+
 
 
 
@@ -52,21 +53,7 @@ def main():
         database.offlinehandler(client.connected_flag, accel, magnet, gyro, alti, client)
     time.sleep(0.1)
 
-    def on_press(key):
-        if key == keyboard.Key.esc:
-            return False  # stop listener
-        try:
-            k = key.char  # single-char keys
-        except:
-            k = key.name  # other keys
-        if k in ['1', '2', 'left', 'right']:  # keys of interest
-            # self.keys.append(k)  # store it in global-like variable
-            print('Key pressed: ' + k)
-            return False  # stop listener; remove this if want more keys
-
-    listener = keyboard.Listener(on_press=on_press)
-    listener.start()  # start to listen on a separate thread
-    listener.join()  # remove if main thread is polling self.keys
+    gui()
 
     while loggedIn == False:
         key = inkey()
