2d75993e0f18ecd6528005db79fdc3d5e0f17dc4
Sinic98 <41592181+Sinic98@users.noreply.github.com>
2020-07-21 16:13:42 +0200

    Update login.py

--- login.py
+++ login.py
@@ -45,7 +45,7 @@ def answer_handler(loggedIn):
     print("Schleife raus")
     print(mqtt_client.loginAnswer)
     print(mqtt_client.loginAnswer["certified"])
-    if mqtt_client.loginAnswer["certified"] == "True":
+    if str(mqtt_client.loginAnswer["certified"]) == "True":
         loggedIn = True
     time.sleep(1)
     return loggedIn
