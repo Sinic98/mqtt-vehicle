003ffc13183d6f82bb20030d0e994256bb71ad9d
Sinic98 <jannik.linke@web.de>
2020-07-21 22:03:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
         database.offlinehandler(client.connected_flag, accel, magnet, gyro, alti, client)
     time.sleep(0.1)
 
-    gui.gui()
+    gui.idle()
 
     while loggedIn == False:
         key = inkey()
