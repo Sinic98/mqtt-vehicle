be5f1d601d1fe4b4c035ecfd2e2be20c13758283
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-22 19:00:21 +0200

    Update database.py

--- database.py
+++ database.py
@@ -8,8 +8,8 @@ def offlinehandler(connected, accel, magnet, gyro, alti, client):
         while not connected:
             json_data = sensors.saveSensorValuesAsJson(accel, magnet, gyro, alti)
             file.write(json_data)
-            time.sleep(1)
-
+            time.sleep(5)
+            connected = getconnected()
         file.close()
         offline_json = open("offline.txt").readlines()
         offline_json_str = str(offline_json)
@@ -19,3 +19,7 @@ def offlinehandler(connected, accel, magnet, gyro, alti, client):
         connected = True
         time.sleep(0.9)
         return
+
+def getconnected():
+    #tbd.
+    return False
