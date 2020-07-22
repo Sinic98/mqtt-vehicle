8bb8df2762681584fe378d177ef17384e6a9790a
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-22 17:25:09 +0200

    Update login.py

--- login.py
+++ login.py
@@ -48,21 +48,22 @@ def answer_handler(loggedIn):
     if str(mqtt_client.loginAnswer["certified"]) == "True" and str(mqtt_client.loginAnswer["login"]) == "True":
         print("login successfull!")
         loggedIn = True
-        return loggedIn
+
+
     elif str(mqtt_client.loginAnswer["certified"]) == "True" and str(mqtt_client.loginAnswer["login"]) == "False":
         print("You are logged out!\nDon't forget your phone and your passengers :)")
         print(" ")
         print(" ")
         loggedIn = False
-        return loggedIn
+
     elif str(mqtt_client.loginAnswer["certified"]) == "False" and str(mqtt_client.loginAnswer["login"]) == "True":
         print("Access denied!")
     elif str(mqtt_client.loginAnswer["certified"]) == "False" and str(mqtt_client.loginAnswer["login"]) == "False":
         print("Logout failed!")
-        loggedIn = False
+
     else:
         print("error with authentification")
-    
+
     x = True
     mqtt_client.loginque = False
     mqtt_client.loginAnswer = None
