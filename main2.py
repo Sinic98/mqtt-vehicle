446735033b3208fe8b64767f7fd951c1f8ab32b0
Erec-John <68231976+Erec-John@users.noreply.github.com>
2020-07-22 18:56:52 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -42,7 +42,7 @@ def sensorvalues():
         if loggout == 'q':
             timestamp = time.time() *1000
             timestampstr = str(timestamp)
-            loggoutmessage = "{\"timestamp\": " + timestampstr + ", \"login\"= false}"
+            loggoutmessage = "{\"timestamp\": " + timestampstr + ", \"login\": false}"
             mqtt_client.publish("/SysArch/V4/com2/web", loggoutmessage, client)
             print("You are logged out! Don't forgett your phone :)")
             print(" ")
